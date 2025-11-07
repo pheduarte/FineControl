43b48f3745a9d009e7f1adc08c72fbdf9ffc6a41
Phelippe Duarte <160677144+pheduarte@users.noreply.github.com>
2025-11-07 11:18:12 +1100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+document.querySelector("#downloadBtn").addEventListener("click", () => {
+  window.open("https://play.google.com/store/apps/details?id=com.pheduarte.finecontrol", "_blank");
+});
+
+document.querySelector("#btnPrivacyPolicy").addEventListener("click", () => {
+  window.open("https://sites.google.com/view/finecontrolappprivacypolicy", "_blank");
+});
+
